230a423f3bfe494198540bf53c4c79697182b613
Arkan Damar <arkan.damar7@gmail.com>
2023-08-02 22:42:01 +0700

    install mongosh and communicate between mongodb and express

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose
+  .connect("mongodb://arkan:arkan@mongo:27017/?authSource=admin")
+  // refer mongodb container ip address by service name in docker compose
+  .then(() => console.log("successfully connected to DB"))
+  .catch((e) => console.log(e));
+
 app.get("/", (req, res) => {
-  res.send("<h2>halo gais yak makasih semuanya</h2>");
+  res.send("<h2>yak begitulah gais</h2>");
 });
 
 const port = process.env.PORT || 3000;
