9fd3da95f1600a7209cbac8e62973b274a4afddb
Arkan Damar <arkan.damar7@gmail.com>
2023-08-04 19:11:09 +0700

    make connection for mongodb based on some environment variable

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASS,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
 mongoose
-  .connect("mongodb://arkan:arkan@mongo:27017/?authSource=admin")
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   // refer mongodb container ip address by service name in docker compose
   .then(() => console.log("successfully connected to DB"))
   .catch((e) => console.log(e));
